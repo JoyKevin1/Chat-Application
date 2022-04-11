40d2b1008f7a8ba12b15e00906af9c8a9478eacf
FullStackKevin <joykevinrobin1@gmail.com>
2022-04-11 22:53:59 +0200

    Changed

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ io.on('connection', socket => {
     //Listen for chatMessage
     socket.on('chatMessage', msg => {
         const user = getCurrentUser(socket.id)
-        io.to(user.room).emit('message', formatMessage('user.username', msg))
+        io.to(user.room).emit('message', formatMessage(user.username, msg))
     })
 
     // Runs when client disconnects
